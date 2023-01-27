70e6009236c994a1606653b1ade34d654f97633b
NILESH KUND <111678618+Nileshkund@users.noreply.github.com>
2023-01-27 21:41:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
-import React, { Component } from "react";
-import Counter from "./counterContainer";
+// import React, { Component } from "react";
+// import Counter from "./counterContainer";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <Counter />
-                </header>
-            </div>
-        );
-    }
-}
-export default App;
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 <header className="App-header">
+//                     <Counter />
+//                 </header>
+//             </div>
+//         );
+//     }
+// }
+// export default App;
+
+import { connect } from "react-redux";
+
+import Counter from "./counter";
+
+import { increment, decrement, reset } from "../actions";
+
+const mapStateToProps = (state) => {
+
+return {
+
+counter: state,
+
+};
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+return {
+
+increment: () => dispatch(increment()),
+
+decrement: () => dispatch(decrement()),
+
+reset: () => dispatch(reset()),
+
+};
+
+};
+
+// add code here
